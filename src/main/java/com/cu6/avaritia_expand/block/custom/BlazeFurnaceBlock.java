c7ce91d6c8b9287de1c7faf96b2e5b310bf58f79
cu6 <3077476144@qq.com>
2025-08-03 20:55:38 +0800

    0.0.2_update

--- src/main/java/com/cu6/avaritia_expand/block/custom/BlazeFurnaceBlock.java
+++ src/main/java/com/cu6/avaritia_expand/block/custom/BlazeFurnaceBlock.java
@@ -1,38 +1,54 @@
 package com.cu6.avaritia_expand.block.custom;
 
+
 import com.cu6.avaritia_expand.block.entity.BlazeFurnaceBlockEntity;
 import com.cu6.avaritia_expand.block.entity.ModBlockEntities;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
 
-public class BlazeFurnaceBlock extends BaseEntityBlock {
-    public static final VoxelShape SHAPE = Block.box(0,0,0,16,16,16);
 
+public class BlazeFurnaceBlock extends BaseEntityBlock {
+    public static final DirectionProperty FACING;
+    public static final BooleanProperty LIT;
+    public static final VoxelShape SHAPE = Block.box(0,0,0,16,16,16);
     public BlazeFurnaceBlock(Properties pProperties) {
         super(pProperties);
+        this.registerDefaultState((BlockState)((BlockState)((BlockState)this.stateDefinition.any()).setValue(FACING, Direction.NORTH)).setValue(LIT, false));
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext ctx) {
+        return (BlockState) this.defaultBlockState().setValue(FACING, ctx.getHorizontalDirection().getOpposite());
     }
 
     @Override
     public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
         return SHAPE;
     }
+
+
 
     @Override
     public RenderShape getRenderShape(BlockState pState) {
@@ -83,5 +99,25 @@ public class BlazeFurnaceBlock extends BaseEntityBlock {
                     }
                 }
         );
+    }
+
+
+
+    public BlockState rotate(BlockState pState, Rotation pRotation) {
+        return (BlockState)pState.setValue(FACING, pRotation.rotate((Direction)pState.getValue(FACING)));
+    }
+
+    public BlockState mirror(BlockState pState, Mirror pMirror) {
+        return pState.rotate(pMirror.getRotation((Direction)pState.getValue(FACING)));
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(new Property[]{FACING, LIT});
+    }
+
+
+    static {
+        FACING = HorizontalDirectionalBlock.FACING;
+        LIT = BlockStateProperties.LIT;
     }
 }
